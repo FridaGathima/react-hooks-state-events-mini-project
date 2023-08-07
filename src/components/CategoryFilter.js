cbf2c99f5289163f93aa79e632077c94d91321b6
FridaGathima <frida.gathima@student.moringaschool.com>
2023-08-07 12:38:55 +0300

    completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, categoryIndex, onSelectedButtonChange}) {
+  const buttons = categories.map((category, index)=> {
+    const selectedClass = categoryIndex=== index ? {className: "selected"} : {}
+  })
   return (
     <div className="categories">
       <h5>Category filters</h5>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,6 +1,10 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, onTaskDelete}) {
+  const taskList = tasks.map((task, index) => (
+    <Task key={index} text={task.text} category={task.category} onTaskDelete={() => onTaskDelete(index)} />
+  ))
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
